060fc236ba33fd0c7e6f73574855de0d4db01936
yuvathejasimha <yuvatheja.s@gmail.com>
2017-07-09 16:29:54 -0500

    app.js ng-route update

--- Frontend/src/js/app.js
+++ Frontend/src/js/app.js
@@ -1,1 +1,38 @@
-suyfcvaug
+var myApp = angular.module('myApp', [
+    'ngRoute',
+    'pageControllers'
+]);
+
+myApp.value('ticket', false);
+
+myApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+	$routeProvider.
+        when('/home', {
+            templateUrl: 'html/home.html',
+            controller: 'LoginController'
+        }).
+        when('/studentlist', {
+            resolve: {
+                "check": function ($location) {
+                    if (!ticket) {
+                        $location.path('/home');
+                    }
+                }
+            },
+            templateUrl: 'html/studentlist.html'
+        }).
+        when('/weeklystatus', {
+            resolve: {
+                "check": function ($location) {
+                    if (!ticket) {
+                        $location.path('/home');
+                    }
+                }
+            },
+            templateUrl: 'html/weeklystatus.html'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+    $locationProvider.html5Mode(false);
+}]);
