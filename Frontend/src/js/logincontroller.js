568e809a1b96dfce17d7772ce689f116d27e7dbb
yuvathejasimha <yuvatheja.s@gmail.com>
2017-07-09 16:33:20 -0500

    logincontroller.js created

---
+++ Frontend/src/js/logincontroller.js
@@ -0,0 +1,19 @@
+var pageControllers = angular.module('pageControllers', []);
+
+
+pageControllers.controller('LoginController', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    $scope.errmessage = '';
+    $scope.login = function () {
+        if ($scope.name) {
+            if (($scope.name == 'admin') && ($scope.password == 'admin')) {
+                ticket = true;
+                $location.path('/studentlist');
+            } else {
+                $scope.errmessage = 'Please enter valid credentials of ' + $scope.Role;
+            }
+        } else {
+            $scope.errmessage = 'Please enter valid credentials of '+$scope.Role;
+        }
+    };
+    
+}]);
